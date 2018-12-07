6c5f4831f7bd6bb04061f5c3ec89f359029b29f0
Walter Almada <39980099+walterioo@users.noreply.github.com>
2018-12-06 19:21:20 -0700

    Added passport support, fixed canary test

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,27 +1,57 @@
-var db = require("../models");
+var db = require("../models"),
+  isAuth = require("../config/middleware/isAuth");
 
 module.exports = function(app) {
   // Load index page
+  // app.get("/", function(req, res) {
+  //   db.Example.findAll({}).then(function(dbExamples) {
+  //     res.render("index", {
+  //       msg: "Welcome!",
+  //       examples: dbExamples
+  //     });
+  //   });
+  // });
+
+  // // Load example page and pass in an example by id
+  // app.get("/example/:id", function(req, res) {
+  //   db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+  //     res.render("example", {
+  //       example: dbExample
+  //     });
+  //   });
+  // });
+
+  // Admin only Routes
+
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
+    // If the user already has an account send them to the members page
+    if (req.user) {
+      res.redirect("/admin");
+    }
+    db.Report.findAll({}).then(function(dbReport) {
+      res.render("index", dbReport);
     });
   });
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
+  app.get("/login", function(req, res) {
+    // If the user already has an account send them to the members page
+    if (req.user) {
+      res.redirect("/admin");
+    }
+    res.sendFile(path.join(__dirname, "../public/login.html"));
   });
+
+  // Here we've add our isAuthenticated middleware to this route.
+  // If a user who is not logged in tries to access this route they will be 
+  //redirected to the signup page
+  app.get("/admin", isAuth, function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/admin.html"));
+  });
+
 
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
     res.render("404");
   });
+
 };
